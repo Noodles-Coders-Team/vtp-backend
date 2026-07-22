fca8f08b02a633a3f8a0917ea2325de7dd736f5f
geggabyte <kolesnikov.genka@gmail.com>
2026-07-22 17:30:08 +0200

    Error messages. Channel data import.

--- src/routes/import.ts
+++ src/routes/import.ts
@@ -5,6 +5,7 @@ import csv from "csv-parser";
 import fs from "fs";
 
 import { importChannelDataCsv, importGameCsv, importTableDataCsv } from "../services/importService";
+
 const router = Router();
 
 
@@ -13,87 +14,102 @@ const upload = multer({ dest: "uploads/" })
 router.post('/games',
     upload.single("file"),
     async (request, response) => {
-        if (!request.file)
-            return response.status(400).json({ error: "No File uploaded" });
+        try {
+            if (!request.file)
+                return response.status(400).json({ error: "No File uploaded" });
 
-        let results: GameCsv[] = [];
-        await new Promise<void>((resolve, reject) => {
-            if (!request.file) {
-                return;
-            }
+            let results: GameCsv[] = [];
+            await new Promise<void>((resolve, reject) => {
+                if (!request.file) {
+                    return;
+                }
 
-            fs.createReadStream(request.file.path)
-                .pipe(csv())
-                .on("data", (data: GameCsv) => results.push(data))
-                .on("end", resolve)
-                .on("error", reject)
-        });
+                fs.createReadStream(request.file.path)
+                    .pipe(csv())
+                    .on("data", (data: GameCsv) => results.push(data))
+                    .on("end", resolve)
+                    .on("error", reject)
+            });
 
-        fs.unlinkSync(request.file!.path);
-        console.log(`Import file read, length: ${results.length}, with first entry: ${results[0]}`)
-        await importGameCsv(results);
-        response.json({
-            rows: results.length,
-            data: results
-        });
+            fs.unlinkSync(request.file!.path);
+            console.log(`Import file read, length: ${results.length}, with first entry: ${results[0]}`)
+
+            await importGameCsv(results);
+            response.status(200).json({
+                rows: results.length,
+                data: results
+            });
+        } catch (e) {
+            response.status(500).json({ message: 'Internal Server Error!', error: e })
+        }
     });
 
 
 router.post('/channel-data',
     upload.single("file"),
     async (request, response) => {
-        if (!request.file)
-            return response.status(400).json({ error: "No File uploaded" });
+        try {
+            if (!request.file)
+                return response.status(400).json({ error: "No File uploaded" });
 
-        let results: ChannelDataCsv[] = [];
-        await new Promise<void>((resolve, reject) => {
-            if (!request.file) {
-                return;
-            }
+            let results: ChannelDataCsv[] = [];
+            await new Promise<void>((resolve, reject) => {
+                if (!request.file) {
+                    return;
+                }
 
-            fs.createReadStream(request.file.path)
-                .pipe(csv())
-                .on("data", (data: ChannelDataCsv) => results.push(data))
-                .on("end", resolve)
-                .on("error", reject)
-        });
+                fs.createReadStream(request.file.path)
+                    .pipe(csv())
+                    .on("data", (data: ChannelDataCsv) => results.push(data))
+                    .on("end", resolve)
+                    .on("error", reject)
+            });
 
-        fs.unlinkSync(request.file!.path);
-        console.log(`Import file read, length: ${results.length}, with first entry: ${results[0]}`)
-        await importChannelDataCsv(results);
-        response.json({
-            rows: results.length,
-            data: results
-        });
+            fs.unlinkSync(request.file!.path);
+            console.log(`Import file read, length: ${results.length}, with first entry: ${results[0]}`)
+
+            await importChannelDataCsv(results);
+            response.status(200).json({
+                rows: results.length,
+                data: results
+            });
+        } catch (e) {
+            response.status(500).json({ message: 'Internal Server Error!', error: e })
+        }
     });
 
-    
+
 router.post('/table-data',
     upload.single("file"),
     async (request, response) => {
-        if (!request.file)
-            return response.status(400).json({ error: "No File uploaded" });
+        try {
+            if (!request.file)
+                return response.status(400).json({ error: "No File uploaded" });
 
-        let results: TableDataCsv[] = [];
-        await new Promise<void>((resolve, reject) => {
-            if (!request.file) {
-                return;
-            }
+            let results: TableDataCsv[] = [];
+            await new Promise<void>((resolve, reject) => {
+                if (!request.file) {
+                    return;
+                }
 
-            fs.createReadStream(request.file.path)
-                .pipe(csv())
-                .on("data", (data: TableDataCsv) => results.push(data))
-                .on("end", resolve)
-                .on("error", reject)
-        });
+                fs.createReadStream(request.file.path)
+                    .pipe(csv())
+                    .on("data", (data: TableDataCsv) => results.push(data))
+                    .on("end", resolve)
+                    .on("error", reject)
+            });
 
-        fs.unlinkSync(request.file!.path);
-        console.log(`Import file read, length: ${results.length}, with first entry: ${results[0]}`)
-        await importTableDataCsv(results);
-        response.json({
-            rows: results.length,
-            data: results
-        });
+            fs.unlinkSync(request.file!.path);
+            console.log(`Import file read, length: ${results.length}, with first entry: ${results[0]}`)
+
+            await importTableDataCsv(results);
+            response.status(200).json({
+                rows: results.length,
+                data: results
+            });
+        } catch (e) {
+            response.status(500).json({ message: 'Internal Server Error!', error: e })
+        }
     });
 
 export default router;
